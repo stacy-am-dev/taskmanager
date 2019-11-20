4e7d26dd9f4a95ecf3595c9b8999ed1b5c715153
pacifica-spiegelman <nastusha8698@gmail.com>
2019-11-20 23:20:56 +0400

    Adjustment class: Task

--- src/Task.java
+++ src/Task.java
@@ -1,85 +1,87 @@
-    import java.util.Date;
+import java.time.LocalTime;
 
-    public class Task {
-        private String status; // enum Status
-        private String id;
-        private String description;
-        private Date supposedTimeExecute;
-        private Date actualTimeExecute;
-        private Date startTime;
-        private String type;
-        private String employee;
-        private int level;
+public class Task {
+    private long status; // enum Status
+    private long id;
+    private long description;
+    private LocalTime supposedTimeExecute;
+    private LocalTime actualTimeExecute;
+    private LocalTime startTime;
+    private long type;
+    private long employee;
+    private int level;
 
-        public String getStatus() {
-            return status;
-        }
+    public Task(){}
 
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public Date getSupposedTimeExecute() {
-            return supposedTimeExecute;
-        }
-
-        public void setSupposedTimeExecute(Date supposedTimeExecute) {
-            this.supposedTimeExecute = supposedTimeExecute;
-        }
-
-        public Date getActualTimeExecute() {
-            return actualTimeExecute;
-        }
-
-        public void setActualTimeExecute(Date actualTimeExecute) {
-            this.actualTimeExecute = actualTimeExecute;
-        }
-
-        public Date getStartTime() {
-            return startTime;
-        }
-
-        public void setStartTime(Date startTime) {
-            this.startTime = startTime;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public String getEmployee() {
-            return employee;
-        }
-
-        public void setEmployee(String employee) {
-            this.employee = employee;
-        }
-
-        public int getLevel() {
-            return level;
-        }
-
-        public void setLevel(int level) {
-            this.level = level;
-        }
+    public long getStatus() {
+        return status;
     }
+
+    public void setStatus(long status) {
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getDescription() {
+        return description;
+    }
+
+    public void setDescription(long description) {
+        this.description = description;
+    }
+
+    public LocalTime getSupposedTimeExecute() {
+        return supposedTimeExecute;
+    }
+
+    public void setSupposedTimeExecute(LocalTime supposedTimeExecute) {
+        this.supposedTimeExecute = supposedTimeExecute;
+    }
+
+    public LocalTime getActualTimeExecute() {
+        return actualTimeExecute;
+    }
+
+    public void setActualTimeExecute(LocalTime actualTimeExecute) {
+        this.actualTimeExecute = actualTimeExecute;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getType() {
+        return type;
+    }
+
+    public void setType(long type) {
+        this.type = type;
+    }
+
+    public long getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(long employee) {
+        this.employee = employee;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
