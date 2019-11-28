558daa47ef4ef9f7794afa9e6d4abaa9b19f0e5c
Darya <dshishkina13@gmail.com>
2019-11-28 08:36:00 +0400

    #2 Class:Skill

--- com/netcracker/taskmanager/src/main/java/tasks/model/Skill.java
+++ com/netcracker/taskmanager/src/main/java/tasks/model/Skill.java
@@ -1,49 +1,117 @@
 package main.java.tasks.model;
 
+/*
+ *Class describes skills of employees with such properties as <b>skillId</b>,
+ * <b>skillName</b>, <b>minLevel</b>, <b>maxLevel</b>, <b>skillDescription</b>.
+ * @version 1.0
+ */
 public class Skill {
-    private long skillID;
-    private String nameSkill;
+    private Long skillId;
+    private String skillName;
     private int minLevel;
     private int maxLevel;
-    private String description;
+    private String skillDescription;
 
-    public long getSkillID() {
-        return skillID;
+    /*
+     *Constructor creates new object with definite values
+     * @param skillId - ID of skill
+     * @param skillName - name of skill
+     * @param minLevel - minimal level of skill
+     * @param maxLevel - maximal level of skill
+     * @see Skill#Skill()
+     */
+    public Skill(Long skillId, String skillName, int min, int max) {
+        this.skillId = skillId;
+        this.skillName = skillName;
+        minLevel = min;
+        maxLevel = max;
     }
 
-    public void setSkillID(long skillID) {
-        this.skillID = skillID;
+    /*
+     *Constructor creates new object
+     * @see Skill#Skill(Long, String, int, int)
+     */
+    public Skill() {
     }
 
-    public String getNameSkill() {
-        return nameSkill;
+
+    /*
+     *Function receives value of field {@link Skill#skillId}
+     *@return skillId
+     */
+    public Long getSkillId() {
+        return skillId;
     }
 
-    public void setNameSkill(String nameSkill) {
-        this.nameSkill = nameSkill;
+    /*
+     *Procedure defines value of field {@link Skill#skillId}
+     *@param skillId
+     */
+    public void setSkillId(Long skillId) {
+        this.skillId = skillId;
     }
 
+    /*
+     *Function receives value of field {@link Skill#skillName}
+     *@return skillName
+     */
+    public String getSkillName() {
+        return skillName;
+    }
+
+    /*
+     *Procedure defines value of field {@link Skill#skillName}
+     *@param skillName
+     */
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
+
+    /*
+     *Function receives value of field {@link Skill#minLevel}
+     *@return minLevel
+     */
     public int getMinLevel() {
         return minLevel;
     }
 
+    /*
+     *Procedure defines value of field {@link Skill#minLevel}
+     *@param minLevel
+     */
     public void setMinLevel(int minLevel) {
         this.minLevel = minLevel;
     }
 
+    /*
+     *Function receives value of field {@link Skill#maxLevel}
+     *@return maxLevel
+     */
     public int getMaxLevel() {
         return maxLevel;
     }
 
+    /*
+     *Procedure defines value of field {@link Skill#maxLevel}
+     *@param maxLevel
+     */
     public void setMaxLevel(int maxLevel) {
         this.maxLevel = maxLevel;
     }
 
-    public String getDescription() {
-        return description;
+    /*
+     *Function receives value of field {@link Skill#skillDescription}
+     *@return skillDescription
+     */
+    public String getSkillDescription() {
+        return skillDescription;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    /*
+     *Procedure defines value of field {@link Skill#skillDescription}
+     *@param skillDescription
+     */
+    public void setSkillDescription(String skillDescription) {
+        this.skillDescription = skillDescription;
     }
 }
