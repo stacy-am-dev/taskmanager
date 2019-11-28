793744cd6a1f4b9bedf9374d5897b0466f0ee5c2
Darya <dshishkina13@gmail.com>
2019-11-28 12:01:02 +0400

    №3 Class: Employee

--- com/netcracker/taskmanager/src/main/java/tasks/model/Employee.java
+++ com/netcracker/taskmanager/src/main/java/tasks/model/Employee.java
@@ -1,71 +1,129 @@
 package main.java.tasks.model;
 
-
-
+/*
+ *Class describes  employees with such properties as <b>employeeId</b>,
+ * <b>email</b>, <b>username</b>, <b>password</b>, <b>firstName</b>, <b>lastName</b>.
+ * @version 1.0
+ */
 public class Employee {
-    private long employeeID;
-    private String email;
+    private Long employeeId;
     private String username;
     private String password;
     private String firstName;
     private String lastName;
+    private String email;
 
+    /*
+     *Constructor creates new object
+     * @see Employee#Employee(String, String)
+     */
     public Employee() {
-
     }
 
+    /*
+     *Constructor creates new object with definite values
+     * @param firstName - name of employee
+     * @param lastName - surname of employee
+     * @see Employee#Employee()
+     */
     public Employee(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public long getEmployeeID() {
-        return employeeID;
+    /*
+     *Function receives value of field {@link Employee#employeeId}
+     *@return employeeId
+     */
+    public Long getEmployeeId() {
+        return employeeId;
     }
 
-    public void setEmployeeID(long employeeID) {
-        this.employeeID = employeeID;
+    /*
+     *Procedure defines value of field {@link Employee#employeeId}
+     *@param employeeId
+     */
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    /*
+     *Function receives value of field {@link Employee#username}
+     *@return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /*
+     *Procedure defines value of field {@link Employee#username}
+     *@param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /*
+     *Function receives value of field {@link Employee#password}
+     *@return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /*
+     *Procedure defines value of field {@link Employee#password}
+     *@param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-
+    /*
+     *Function receives value of field {@link Employee#firstName}
+     *@return firstName
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /*
+     *Procedure defines value of field {@link Employee#firstName}
+     *@param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /*
+     *Function receives value of field {@link Employee#LastName}
+     *@return lastName
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /*
+     *Procedure defines value of field {@link Employee#lastName}
+     *@param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /*
+     *Function receives value of field {@link Employee#email}
+     *@return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /*
+     *Procedure defines value of field {@link Employee#email}
+     *@param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
