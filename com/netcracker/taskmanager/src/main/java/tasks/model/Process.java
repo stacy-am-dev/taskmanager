4ce8d4b2b9b98c81e6da2a0771d09f7011f3bcac
DariyaKozlovskaya <darya.kozlovskaya.98@inbox.ru>
2019-11-28 13:46:36 +0400

    #5JavaDoc added ProcessStatus changed to enum

--- com/netcracker/taskmanager/src/main/java/tasks/model/Process.java
+++ com/netcracker/taskmanager/src/main/java/tasks/model/Process.java
@@ -4,73 +4,154 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.time.LocalDateTime;
 
-
+/**
+ * Describes the workflow. Process contains a description, name, starDate, endDate,excpectaionStartDate,excpectaionEndDate,status
+ */
 public class Process {
 
+    /**
+     * process identifier
+     */
     private long processId;
+    /**
+     * process description
+     */
     private String description;
+    /**
+     *  process name
+     */
     private String name;
-    private LocalDateTime starDate;
+    /**
+     * process starDate
+     */
+    private LocalDateTime startDate;
+    /**
+     * process endDate
+     */
     private LocalDateTime endDate;
+    /**
+     * process excpectionStarDate
+     */
     private LocalDateTime excpectaionStartDate;
+    /**
+     * Process excpectaionEndDate
+     */
     private LocalDateTime excpectaionEndDate;
-    private String status;
+    /**
+     * Process status
+     */
+    private ProcessStatus status;
+    /**
+       *Constructor creates new object with definite values
+     * @param processId - ID of process
+     * @param description - description of process
+     * @param name - name of process
+     * @param startDate - starDate of process
+     * @param endDate - endDate of process
+     * @param exceptionStartDate- exceptionStartDate of process
+     * @param exceptionEndDate-exceptionsEndDate of process
+     * @see Process#Process()
+     */
+    public Process(long processId,String description,String name,LocalDateTime startDate, LocalDateTime endDate, LocalDateTime exceptionStartDate,LocalDateTime exceptionEndDate){
+       this.processId=processId;
+       this.description=description;
+       this.name=name;
+       this.startDate=startDate;
+       this.endDate=endDate;
+       this.excpectaionStartDate=exceptionStartDate;
+       this.excpectaionEndDate=exceptionEndDate;
+
+    }
+    /**
+     * Constructor creates new object
+     */
     public Process(){
 
     }
+    /**
+     *Function receives value of field{@link Process#processId}
+     *@return processId
+     */
+
     public long getprocessId() {
         return processId;
     }
+    /**
+     *Function defines value of field {@link Process#processId}
+     *@param processId
+     */
 
     public void setId(long processId) {
         this.processId = processId;
     }
+    /**
+     *Function receives value of description{@link Process#description}
+     *@return description
+     */
 
     public String getDescription() {
         return description;
     }
-
+    /**
+     *Function defines value of field {@link Process#description}
+     *@param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
-
+    /**
+     *Function defines value of field {@link Process#name}
+     *@param name
+     */
     public void setName(String name){
         this.name=name;
     }
-
+    /**
+     *Function receives value of description{@link Process#name}
+     *@return name
+     */
     public String getName(){
         return name;
     }
-    public void setStatus(String status){
-        this.status=status;
-    }
-    public String getStatus(){
-        return status;
+
+    public LocalDateTime getExceptionStartDate() {
+        return excpectaionStartDate;
     }
 
-    public LocalDateTime getSupposedTimeExecute() {
-        return supposedTimeExecute;
+    public void setExceptionStartDate(LocalDateTime exceptionStartDate) {
+        this.excpectaionStartDate = exceptionStartDate;
     }
 
-    public void setSupposedTimeExecute(LocalDateTime supposedTimeExecute) {
-        this.supposedTimeExecute = supposedTimeExecute;
+    public LocalDateTime getExcpectaionEndDate() {
+        return excpectaionEndDate;
     }
 
-    public LocalDateTime getActualTimeExecute() {
-        return actualTimeExecute;
+    /**
+     * Function of setting the actual runtime
+     * @param excpectaionEndDate runtime
+
+     */
+    public void setExcpectaionEndDate(LocalDateTime excpectaionEndDate) {
+        this.excpectaionEndDate = excpectaionEndDate;
     }
 
-    public void setActualTimeExecute(LocalDateTime actualTimeExecute) {
-        this.actualTimeExecute = actualTimeExecute;
+    /**
+     *Function of obtaining start time procecss
+      * @return starDate the start time of the process
+     */
+    public LocalDateTime getStartDate(){
+        return startDate;
+    }
+    public void setStarDate(LocalDateTime startDate){
+        this.startDate=startDate;
+    }
+    public LocalDateTime getEndDate(){
+        return endDate;
+    }
+    public void setEndDate(LocalDateTime endDate){
+        this.endDate=endDate;
     }
 
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalDateTime startTime) {
-        this.startTime = startTime;
-    }
     public void saveToFile(OutputStream out){
 
     }
---
+++ com/netcracker/taskmanager/src/main/java/tasks/model/ProcessStatus.java
@@ -0,0 +1,5 @@
+package main.java.tasks.model;
+
+public enum ProcessStatus {
+    CREATED, STARTED, FINISHED, DELAYED
+}
