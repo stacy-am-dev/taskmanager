f6a4b0c8072d3f6168a5c407dfc7ce8c18f6b4c2
pacifica-spiegelman <nastusha8698@gmail.com>
2019-12-16 16:05:50 +0400

    #16: change constructor and variable type in TaskManagerException

--- src/main/java/com/netcracker/taskmanager/exception/TaskManagerException.java
+++ src/main/java/com/netcracker/taskmanager/exception/TaskManagerException.java
@@ -1,18 +1,29 @@
 package com.netcracker.taskmanager.exception;
 
 /**
- * Class describes exception of task manager  with such properties as <b>codeException</b>
+ * Class describes exception of task manager  with such properties as <b>errorCode</b>
  */
 public class TaskManagerException extends Exception {
     /**
      * Exception code
      */
-    Throwable codeException;
+    private int errorCode;
+
     /**
      * Constructor creates new object
      * @param cause
+     * @param errorCode
      */
-    public TaskManagerException(Throwable cause) {
+    public TaskManagerException(Throwable cause, int errorCode) {
         super(cause);
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * Function receives value of field {@link TaskManagerException#errorCode}
+     * @return errorCode
+     */
+    public int getErrorCode() {
+        return errorCode;
     }
 }
