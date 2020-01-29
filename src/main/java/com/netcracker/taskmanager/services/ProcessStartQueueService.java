faca7fd53c7718e642cd56b65c47dd146ee7dca8
DariyaKozlovskaya <darya.kozlovskaya.98@inbox.ru>
2020-01-29 16:49:55 +0400

    #25: Create ProcessStartQueueService

---
+++ src/main/java/com/netcracker/taskmanager/services/ProcessStartQueueService.java
@@ -0,0 +1,8 @@
+package com.netcracker.taskmanager.services;
+
+import java.util.concurrent.PriorityBlockingQueue;
+import com.netcracker.taskmanager.model.Process;
+
+public class ProcessStartQueueService extends PriorityBlockingQueue<Process> {
+
+}
