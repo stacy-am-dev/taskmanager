78206d4cc5758cd81c1099d92cc0dd722092df74
pacifica-spiegelman <nastusha8698@gmail.com>
2020-02-05 22:47:54 +0400

    #14: change SkillControllerInterface

--- src/main/java/com/netcracker/taskmanager/controller/SkillControllerInterface.java
+++ src/main/java/com/netcracker/taskmanager/controller/SkillControllerInterface.java
@@ -3,22 +3,23 @@ package com.netcracker.taskmanager.controller;
 import com.netcracker.taskmanager.exception.TaskManagerException;
 import com.netcracker.taskmanager.model.Skill;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface SkillControllerInterface {
 
     /**
      * Function creates new skill
-     */
-    Skill createSkill();
-
-    /**
-     * Function update skill
-     * @param skillId
      * @param skill
      * @return Skill
      */
-    Skill updateSkill(Long skillId, Skill skill);
+    Skill createSkill(Skill skill) throws TaskManagerException;
+
+    /**
+     * Function update skill
+     * @param skill
+     * @return Skill
+     */
+    Skill updateSkill(Skill skill) throws TaskManagerException;
 
     /**
      * Function delete skill
@@ -39,18 +40,26 @@ public interface SkillControllerInterface {
      * @param skillId
      * @return Skill
      */
-    Skill getSkill(Long skillId);
+    Skill getSkillById(Long skillId) throws TaskManagerException;
 
     /**
      * Function receives skill
      * @param skillName
      * @return Skill
      */
-    Skill getSkill(String skillName);
+    Skill getSkillByName(String skillName) throws TaskManagerException;
+
+    /**
+     * Function receives skill
+     * @param skillName
+     * @return Collection
+     * @throws TaskManagerException
+     */
+    Collection getSkillsByName(String skillName) throws TaskManagerException;
 
     /**
      * Function receives list of skills
-     * @return List<Skill>
+     * @return Collection
      */
-    List<Skill> getAllSkill();
+    Collection getAllSkill() throws TaskManagerException;
 }
