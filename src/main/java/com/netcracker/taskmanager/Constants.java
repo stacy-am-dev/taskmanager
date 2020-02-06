93346e5131910fc3148afc146da1a8253af766e0
Darya <dshishkina13@gmail.com>
2020-02-06 14:25:36 +0400

    #31 ProcessTemplateRule

--- src/main/java/com/netcracker/taskmanager/Constants.java
+++ src/main/java/com/netcracker/taskmanager/Constants.java
@@ -3,5 +3,6 @@ package com.netcracker.taskmanager;
 public class Constants {
     public static final int CONTROLLER_PROVIDER_INITIALIZATION_ERROR = 0;
     public static final int GET_CONTROLLER_ERROR = 1;
+    public static final int TASK_INITIALIZATION_ERROR = 2;
   ;  public static final int NO_SUCH_EMPLOYEE = 7;
 }
