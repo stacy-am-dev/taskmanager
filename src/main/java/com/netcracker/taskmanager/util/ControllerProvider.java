b53a0ecd75ac17455cb4d622ac2014c5de955495
Darya <dshishkina13@gmail.com>
2020-02-06 13:40:43 +0400

    #17 ControllerProvider was changed

--- src/main/java/com/netcracker/taskmanager/util/ControllerProvider.java
+++ src/main/java/com/netcracker/taskmanager/util/ControllerProvider.java
@@ -3,38 +3,30 @@ package com.netcracker.taskmanager.util;
 import com.netcracker.taskmanager.exception.TaskManagerException;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import static com.netcracker.taskmanager.Constants.*;
 
 public final class ControllerProvider {
     private static ControllerProvider instance;
-    private static HashMap<Class,String> controllers;
+    private Map<Class, Class> controllers;
 
-    private ControllerProvider()  throws TaskManagerException {
-        try {
-        FileInputStream fis;
-        Properties property = new Properties();
-        fis = new FileInputStream("src/main/resources/controllers.properties");
-        property.load(fis);
+    private ControllerProvider() throws TaskManagerException {
+        try (FileInputStream fis = new FileInputStream("src/main/resources/controllers.properties");) {
+            Properties property = new Properties();
+            property.load(fis);
 
-        for (String key : property.stringPropertyNames())
-        {
-            Class cl = Class.forName(key);
-            controllers.put(cl,property.getProperty(key));
-        }
+            for (String key : property.stringPropertyNames()) {
+                Class cl = Class.forName(key);
+                controllers.put(cl, Class.forName(property.getProperty(key)));
+            }
 
-        } catch (FileNotFoundException e) {
-            throw new TaskManagerException(e, FILE_NOT_FOUND_EXCEPTION);
-        } catch (ClassNotFoundException e) {
-            throw new TaskManagerException(e, CLASS_NOT_FOUND_EXCEPTION);
-        } catch (IOException e) {
-            throw new TaskManagerException(e, IOEXCEPTION);
+        } catch (ClassNotFoundException | IOException e) {
+            throw new TaskManagerException(e, CONTROLLER_PROVIDER_INITIALIZATION_ERROR);
         }
     }
 
@@ -45,23 +37,14 @@ public final class ControllerProvider {
         return instance;
     }
 
-    public static <T> T getController(Class <T> Controller) throws TaskManagerException {
+    public <T> T getController(Class<T> Controller) throws TaskManagerException {
         try {
-            String controllerImplementation = controllers.get(Controller);
-            Class<T> cl = (Class<T>) Class.forName(controllerImplementation);
-            Constructor<T> cons = cl.getDeclaredConstructor();
-            return cons.newInstance();
-        }
-        catch (InstantiationException e) {
-            throw new TaskManagerException(e, INSTANTIATION_EXCEPTION);
-        } catch (InvocationTargetException e) {
-            throw new TaskManagerException(e, INVOCATION_TARGET_EXCEPTION);
-        } catch (NoSuchMethodException e) {
-            throw new TaskManagerException(e, NO_SUCH_METHOD_EXCEPTION);
-        } catch (IllegalAccessException e) {
-            throw new TaskManagerException(e, ILLEGAL_ACCESS_EXCEPTION);
-        } catch (ClassNotFoundException e) {
-            throw new TaskManagerException(e, CLASS_NOT_FOUND_EXCEPTION);
+            Class cl = this.controllers.get(Controller);
+            Constructor cons = cl.getDeclaredConstructor();
+            return (T) cons.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new TaskManagerException(e, GET_CONTROLLER_ERROR);
         }
     }
 }
+
