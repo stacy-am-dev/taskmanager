15c292b252a76569b6e658db535f9b19c99a9525
Darya <dshishkina13@gmail.com>
2020-02-06 15:20:56 +0400

    # 15 EmployeeControllerInterface was changed

--- src/main/java/com/netcracker/taskmanager/controller/EmployeeControllerInterface.java
+++ src/main/java/com/netcracker/taskmanager/controller/EmployeeControllerInterface.java
@@ -2,22 +2,33 @@ package com.netcracker.taskmanager.controller;
 
 import com.netcracker.taskmanager.exception.TaskManagerException;
 import com.netcracker.taskmanager.model.Employee;
+import com.netcracker.taskmanager.model.EmployeeSkill;
 
 import java.util.Collection;
 
 public interface EmployeeControllerInterface{
     /**
-     *Function creates and receives new employee
+     *Function creates and receives employee
+     *@param employee
+     *@param employeeSkills
+     *@return Employee
      */
-    Employee createEmployee(Employee employee) throws TaskManagerException;
+    Employee createEmployee(Employee employee, Collection<EmployeeSkill> employeeSkills ) throws TaskManagerException;
     /**
-     *Function updates employee
+     *Function updates and receives employee
      *@param employee
      *@return Employee
      */
     Employee updateEmployee ( Employee employee) throws TaskManagerException;
     /**
-     *Procedure deletes employee
+     *Function updates and receives employee
+     *@param employee
+     *@param employeeSkills
+     *@return Employee
+     */
+    Employee updateEmployee ( Employee employee,Collection<EmployeeSkill> employeeSkills) throws TaskManagerException;
+    /**
+     *Procedure deletes employee by identifier
      *@param employeeId
      */
     void deleteEmployee(Long employeeId) throws TaskManagerException;
@@ -27,20 +38,26 @@ public interface EmployeeControllerInterface{
      */
     void deleteEmployee(Employee employee) throws TaskManagerException;
     /**
-     *Function receives employee
+     *Function receives employee by identifier
      *@param employeeId
      *@return Employee
      */
     Employee getEmployeeByEmployeeId(Long employeeId) throws TaskManagerException;
     /**
-     *Function receives employee
+     *Function receives employee by username
      *@param username
      *@return Employee
      */
     Employee getEmployeeByUsername(String username) throws TaskManagerException;
     /**
      *Function receives list of employees
-     *@return List<Employee>
+     *@return Collection<Employee>
      */
     Collection<Employee> getAllEmployees() throws TaskManagerException;
+    /**
+     *Function receives list of employee's skills by employee's identifier
+     *@param employeeId
+     *@return Collection<EmployeeSkill>
+     */
+    Collection<EmployeeSkill> getEmployeeSkillsByEmployeeId(Long employeeId);
 }
