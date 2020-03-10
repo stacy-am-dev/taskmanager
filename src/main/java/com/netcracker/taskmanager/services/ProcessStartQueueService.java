c6456d3ad6c04639145921b2e17ef8c3acec463f
Pacifica <nastusha8698@gmail.com>
2020-03-10 12:18:33 +0400

    #25: create ProcessStartQueueService

--- src/main/java/com/netcracker/taskmanager/services/ProcessStartQueueService.java
+++ src/main/java/com/netcracker/taskmanager/services/ProcessStartQueueService.java
@@ -1,27 +1,36 @@
 package com.netcracker.taskmanager.services;
 
-import java.util.concurrent.PriorityBlockingQueue;
 import com.netcracker.taskmanager.model.Process;
 
-public class ProcessStartQueueService  {
-    private static PriorityBlockingQueue<Process> priorityBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 
-    private ProcessStartQueueService(){
+public class ProcessStartQueueService {
+    private static ProcessStartQueueService processStartQueueService;
+    private PriorityBlockingQueue<Process> priorityBlockingQueue;
 
+    private ProcessStartQueueService() {
+        priorityBlockingQueue = new PriorityBlockingQueue<>(1, ((o1, o2) -> {
+            if (o1.getStartDate().isAfter(o2.getStartDate()))
+                return 1;
+            else if (o1.getStartDate().isBefore(o2.getStartDate()))
+                return -1;
+            else
+                return 0;
+        }));
     }
 
-    public static synchronized PriorityBlockingQueue<Process> getPriorityBlockingQueue(){
-        if (priorityBlockingQueue == null)
-            priorityBlockingQueue = new PriorityBlockingQueue<Process>();
-        return priorityBlockingQueue;
+    public static synchronized ProcessStartQueueService getProcessStartQueueService() {
+        if (processStartQueueService == null)
+            processStartQueueService = new ProcessStartQueueService();
+        return processStartQueueService;
     }
 
-    public void add(Process process){
+    public void addProcess(Process process) {
         priorityBlockingQueue.add(process);
     }
 
-    public Process get(){
-        return priorityBlockingQueue.peek();
+    public Process getProcess() {
+        return priorityBlockingQueue.poll();
     }
 
 }
