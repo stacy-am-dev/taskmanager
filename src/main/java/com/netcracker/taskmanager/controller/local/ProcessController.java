e1c132801f0b2fc6075862a74b0517a712cb9dcb
Pacifica <nastusha8698@gmail.com>
2020-04-08 17:10:46 +0400

    #38: fixed methods of ProcessController

--- src/main/java/com/netcracker/taskmanager/controller/local/ProcessController.java
+++ src/main/java/com/netcracker/taskmanager/controller/local/ProcessController.java
@@ -4,85 +4,72 @@ import com.netcracker.taskmanager.controller.ProcessControllerInterface;
 import com.netcracker.taskmanager.exception.TaskManagerException;
 import com.netcracker.taskmanager.model.Process;
 import com.netcracker.taskmanager.model.ProcessStatus;
-import com.netcracker.taskmanager.util.Model;
 import com.netcracker.taskmanager.util.ModelFacade;
 
 import java.util.Collection;
 import java.util.concurrent.PriorityBlockingQueue;
 
-import static com.netcracker.taskmanager.Constants.*;
+import static com.netcracker.taskmanager.Constants.FIELDS_OF_PROCESS_INCORRECT;
+import static com.netcracker.taskmanager.Constants.NO_SUCH_PROCESS;
 
 public class ProcessController implements ProcessControllerInterface {
 
-    private Model getModel = ModelFacade.getInstance().getModel();
-
     @Override
     public Process createProcess(Process process) throws TaskManagerException {
-        if(process.getStartDate().compareTo(process.getEndDate()) > 0)
-            throw new TaskManagerException(new Throwable(""), START_OR_END_DATE_OF_PROCESS_INCORRECT);
-        else{
-            process.setProcessId(getModel.getMatchMap().get(Process.class).generate());
-            getModel.getProcesses().add(process);
-            return process;
-        }
+        if((process.getStartDate().compareTo(process.getEndDate()) > 0) || (ModelFacade.getInstance().getModel().getProcesses().stream().anyMatch(process1 -> process1.getName().equals(process.getName()))))
+            throw new TaskManagerException(new Throwable(""), FIELDS_OF_PROCESS_INCORRECT);
+        process.setProcessId(ModelFacade.getInstance().getModel().getMatchMap().get(Process.class).generate());
+        ModelFacade.getInstance().getModel().getProcesses().add(process);
+        return process;
     }
 
     @Override
     public Process updateProcess(Process process) throws TaskManagerException {
-        if(process.getStartDate().compareTo(process.getEndDate()) > 0)
-            throw new TaskManagerException(new Throwable(""), START_OR_END_DATE_OF_PROCESS_INCORRECT);
-        else {
-            getModel.getProcesses().removeIf(process1 -> process1.getProcessId().equals(process.getProcessId()));
-            getModel.getProcesses().add(process);
-            return process;
-        }
+        if((process.getStartDate().compareTo(process.getEndDate()) > 0) || (ModelFacade.getInstance().getModel().getProcesses().stream().anyMatch(process1 -> process1.getName().equals(process.getName()))))
+            throw new TaskManagerException(new Throwable(""), FIELDS_OF_PROCESS_INCORRECT);
+        return ModelFacade.getInstance().getModel().getProcesses().stream()
+                    .filter(process1 -> process1.getProcessId() == process.getProcessId())
+                    .peek(process1 -> process1 = process)
+                    .findAny()
+                    .orElseThrow(() -> new TaskManagerException(new Throwable(""), NO_SUCH_PROCESS));
+
     }
 
     @Override
     public void deleteProcess(Long processId) throws TaskManagerException {
-        if(processId == null)
-            throw new TaskManagerException(new Throwable(""), NO_SUCH_IDENTIFIER_OF_PROCESS);
-        else getModel.getProcesses().removeIf(process -> process.getProcessId().equals(processId));
+        ModelFacade.getInstance().getModel().getProcesses().removeIf(process -> process.getProcessId() == processId);
     }
 
     @Override
     public void deleteProcess(Process process) throws TaskManagerException {
-        if(process.getStartDate().compareTo(process.getEndDate()) > 0)
-            throw new TaskManagerException(new Throwable(""), START_OR_END_DATE_OF_PROCESS_INCORRECT);
-        else getModel.getProcesses().removeIf(process1 -> process1.equals(process));
+        ModelFacade.getInstance().getModel().getProcesses().remove(process);
     }
 
     @Override
     public Process getProcessByProcessId(Long processId) throws TaskManagerException {
-        Process processResult = getModel.getProcesses().stream()
+        return ModelFacade.getInstance().getModel().getProcesses().stream()
                 .filter(process -> process.getProcessId().equals(processId))
                 .findAny()
-                .orElse(null);
-        if (processResult == null)
-            throw new TaskManagerException(new Throwable(""), NO_SUCH_PROCESS);
-        return processResult;
+                .orElseThrow(() -> new TaskManagerException(new Throwable(""), NO_SUCH_PROCESS));
     }
 
     @Override
     public Process getProcessByProcessName(String processName) throws TaskManagerException {
-        Process processResult = getModel.getProcesses().stream()
+        return ModelFacade.getInstance().getModel().getProcesses().stream()
                 .filter(process -> process.getName().equals(processName))
                 .findAny()
-                .orElse(null);
-        if(processResult == null)
-            throw new TaskManagerException(new Throwable(""), NO_SUCH_PROCESS);
-        return processResult;
+                .orElseThrow(() -> new TaskManagerException(new Throwable(""), NO_SUCH_PROCESS));
     }
 
     @Override
     public Collection<Process> getAllProcesses() throws TaskManagerException {
-        return getModel.getProcesses();
+        return ModelFacade.getInstance().getModel().getProcesses();
     }
 
     @Override
-    public Collection<Process> getProcessesByStatus(PriorityBlockingQueue<Process> processPriorityBlockingQueue, ProcessStatus status) throws TaskManagerException {
+    public Collection<Process> getProcessesByStatus(ProcessStatus status) throws TaskManagerException {
         PriorityBlockingQueue<Process> priorityBlockingQueue = new PriorityBlockingQueue<>();
-        for (Process process : processPriorityBlockingQueue) {
+        for (Process process : ModelFacade.getInstance().getModel().getProcesses()) {
             if (process.getStatus() == status) {
                 priorityBlockingQueue.add(process);
             }
