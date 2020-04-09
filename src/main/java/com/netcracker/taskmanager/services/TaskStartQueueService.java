a3b39daa888637e627fe2d46d0c8b9a79039222a
Pacifica <nastusha8698@gmail.com>
2020-04-09 16:53:14 +0400

    #27: change parameter in getController to TaskControllerInterface, add JavaDoc

--- src/main/java/com/netcracker/taskmanager/services/TaskStartQueueService.java
+++ src/main/java/com/netcracker/taskmanager/services/TaskStartQueueService.java
@@ -1,6 +1,6 @@
 package com.netcracker.taskmanager.services;
 
-import com.netcracker.taskmanager.controller.local.TaskController;
+import com.netcracker.taskmanager.controller.TaskControllerInterface;
 import com.netcracker.taskmanager.exception.TaskManagerException;
 import com.netcracker.taskmanager.model.Task;
 import com.netcracker.taskmanager.model.TaskStatus;
@@ -8,25 +8,55 @@ import com.netcracker.taskmanager.util.ControllerProvider;
 
 import java.util.concurrent.PriorityBlockingQueue;
 
+/**
+ * Class describes start service of tasks's queue with such properties as <b>taskStartQueueService</b>,
+ * <b>priorityBlockingQueue</b>
+ */
 public class TaskStartQueueService {
+    /**
+     * Static variable of type TaskStartQueueService
+     */
     private static TaskStartQueueService taskStartQueueService;
+    /**
+     * Queue of tasks with priority
+     */
     private PriorityBlockingQueue<Task> priorityBlockingQueue;
 
+    /**
+     * Private constructor restricted to this class itself
+     * Create tasks's queue with priority
+     * @throws TaskManagerException
+     */
     private TaskStartQueueService() throws TaskManagerException {
         priorityBlockingQueue = new PriorityBlockingQueue<>(1, (o1, o2) -> Integer.compare(o2.getPriority().getTaskPriority(), o1.getPriority().getTaskPriority()));
-        priorityBlockingQueue.addAll(ControllerProvider.getControllerProvider().getController(TaskController.class).getTasksByStatus(TaskStatus.PLANNED));
+        priorityBlockingQueue.addAll(ControllerProvider.getControllerProvider().getController(TaskControllerInterface.class).getTasksByStatus(TaskStatus.PLANNED));
     }
 
+    /**
+     * Static method to create instance of TaskStartQueueService class
+     * @return taskStartQueueService
+     * @throws TaskManagerException
+     */
     public static synchronized TaskStartQueueService getTaskStartQueueService() throws TaskManagerException {
         if (taskStartQueueService == null)
             taskStartQueueService = new TaskStartQueueService();
         return taskStartQueueService;
     }
 
-    public void addTask(Task task) {
+    /**
+     * Method to add task to queue of tasks
+     * @param task
+     * @throws TaskManagerException
+     */
+    public void addTask(Task task) throws TaskManagerException {
         priorityBlockingQueue.add(task);
     }
 
+    /**
+     * Method to receive the head task from tasks's queue
+     * @return Process
+     * @throws TaskManagerException
+     */
     public synchronized Task getTask() throws TaskManagerException {
         return priorityBlockingQueue.poll();
     }
